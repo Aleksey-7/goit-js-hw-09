775ed3ee98888f7f776ce758d7151a8fd38e614c
Aleksey-7 <84351678+Aleksey-7@users.noreply.github.com>
2022-11-25 22:26:30 +0200

    update color-switcher.js
    create color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,51 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+class ColorSwitcher {
+  constructor({ onChangeColor }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onChangeColor = onChangeColor;
+  }
+
+  onShowStart() {
+    if (this.isActive) {
+      return;
+    }
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      const color = getRandomHexColor();
+
+      this.onChangeColor(color);
+    }, 1000);
+  }
+
+  onShowStop() {
+    refs.stopBtn.disabled = true;
+    refs.startBtn.disabled = false;
+
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  }
+}
+
+function onChangeBodyBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+const colorSwitcher = new ColorSwitcher({
+  onChangeColor: onChangeBodyBackgroundColor,
+});
+
+refs.startBtn.addEventListener('click', () => colorSwitcher.onShowStart());
+refs.stopBtn.addEventListener('click', () => colorSwitcher.onShowStop());
